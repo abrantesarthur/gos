bdd1f864d3b8d29bd39e7160cc141c222bbae2e4
Arthur Abrantes <arthur.abrantes@wildlifestudios.com>
2020-09-13 10:17:27 -0300

    create printh routine

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
-.PHONY: run
-run: main.asm
+main.bin: main.asm printf.asm load_disk.asm
 	nasm -f bin main.asm -o main.bin
-	qemu-system-x86_64 -machine pc main.bin
 
+.PHONY: run
+run: main.bin 
+	qemu-system-x86_64 main.bin
 
+.PHONY: clean
+clean:
+	if [rm main.bin
--- main.asm
+++ main.asm
@@ -1,13 +1,46 @@
-; BOOT_SECTOR: the first 512 bytes are read into
-; memory by the boot loader
-
 [org 0x7c00]
+[bits 16]
+
+; ---------------------------------------------------
+; BOOT_SECTOR: the first 512 bytes are read into
+; ---------------------------------------------------
+section .text				; entrypoints
+	global main
+
+
+main:
+
+cli							; clear interrupts
+jmp 0x0000:ZeroSeg
+ZeroSeg:					; clear segment registers
+	xor ax, ax
+	mov ss, ax
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov sp, main
+	cld						; clear direction flag
+sti							; reinstate interrupts
+
+
+push ax
+xor ax, ax					; reset disk
+mov dl, 0x80
+int 0x13
+pop ax
+
+mov dx, [0x7c00 + 510] 
+call printh
+jmp $
+
 
 mov al, 1					; amount of sectors to read
 mov cl, 2					; start reading from second sector
 call load_disk				; read disk 
-jmp second_sector			; executes code read from second sector
 
+
+%include "printh.asm"
 %include "printf.asm"
 %include "load_disk.asm"
 
@@ -15,8 +48,8 @@ times 510-($-$$) db 0
 dw 0xaa55
 
 ; _________________________________________________
-; SECOND SECTOR: read by load_disk routine 
-; _________________________________________________
+; SECOND SECTOR: read by load_disk routine			
+; -------------------------------------------------
 
 second_sector:
 	mov bx, TEST_STR
